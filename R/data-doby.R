4296a9ab90833a49e1ad1fd5c366b4e91386c7fa
Søren Højsgaard <sorenh@math.aau.dk>
2020-09-12 19:06:26 +0200

    sub_seq etc added

--- R/data-doby.R
+++ R/data-doby.R
@@ -628,3 +628,24 @@
 "potatoes"
 
 
+#' Mathematics marks for students
+#' 
+#' The \code{mathmark} data frame has 88 rows and 5 columns.
+#'
+#' @name data-mathmark
+#' 
+#' @aliases mathmark math
+#' @format This data frame contains the following columns: mechanics, vectors,
+#' algebra, analysis, statistics.
+#' @author Søren Højsgaard, \email{sorenh@@math.aau.dk}
+#' @references David Edwards, An Introduction to Graphical Modelling, Second
+#' Edition, Springer Verlag, 2000
+#' @keywords datasets
+#' @usage data(mathmark)
+#' 
+#' @examples
+#' 
+#' data(mathmark)
+#' 
+"mathmark"
+"math"
