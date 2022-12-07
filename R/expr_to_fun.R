6e5b04d94c7bbe5eb57009ff98415d9ee5d9d106
Søren Højsgaard <sorenh@math.aau.dk>
2022-12-07 12:00:40 +0100

    SH: SH

--- R/expr_to_fun.R
+++ R/expr_to_fun.R
@@ -1,20 +1,99 @@
 #' Convert expression into function object.
 #'
-#' @param e expression
+#' @param expr_ R expression.
+#' @param vec_arg should the function take vector valued argument
 #'
 #' @examples
 #'
 #' ee <- expression(b1 + (b0 - b1)*exp(-k*x) + b2*x)
 #' ff <- expr_to_fun(ee)
+#'
+#' ee <- expression(matrix(c(x1+x2, x1-x2, x1^2+x2^2, x1^3+x2^3),nrow=2))
+#' ff <- expr_to_fun(ee)
+#'
+#' ee <- expression(
+#'   matrix(
+#'     c(8 * x1 * (4 * x1^2 - 625 * x2^2 - 2 * x2
+#'     - 1) + 9 * x1 - 20 * x2 * (x3 + 0.473809523809524 + exp(-x1 *
+#'     x2)/20) * exp(-x1 * x2) - 3 * cos(x2 * x3) - 4.5,
+#'     -20 * x1 * (x3 + 0.473809523809524 + exp(-x1 * x2)/20) * exp(-x1 * x2) + 3 * x3 *
+#'     (x1 - cos(x2 * x3)/3 - 0.5) * sin(x2 * x3) + (-1250 * x2 - 2) * (4
+#'     * x1^2 - 625 * x2^2 - 2 * x2 - 1),
+#'     3 * x2 * (x1 - cos(x2 * x3)/3 -
+#'     0.5) * sin(x2 * x3) + 400 * x3 + 189.52380952381 + 20 * exp(-x1 *
+#'     x2)), nrow = 3))
+#' ff <- expr_to_fun(ee)
+#' 
 #' @export
-expr_to_fun <- function(e){
-    vn <- all.vars(e)
-    fmls <- vector("list", length(vn))
-    names(fmls) <- vn
-    
-    out <- function(){}
+expr_to_fun <- function(expr_, vec_arg=FALSE){
 
-    formals(out) <- fmls
-    body(out) <- e
-    return(out)
+    if (vec_arg){
+        expr_to_one_param_fun(expr_)
+    } else {
+        expr_to_multi_param_fun(expr_)
+    }
+    
 }
+
+expr_to_one_param_fun <- function(e){
+    nms <- all.vars(e)
+    e_str <- expr_to_string()
+    
+    if (length(nms)) {
+        aux <- sapply(1:length(nms),
+                      function(i) {
+                          nm <- nms[i]
+                          paste0(nm, " = parm[", i, "]")
+                      }
+                      )
+    } else {
+        aux <- NULL
+    }
+
+    comb <- c(aux, e_str)    
+    
+    fun_str <- "function(parm)"
+    
+    bd <- paste0("\n{ \n", paste0(comb, collapse=";\n "), "\n}")
+
+    ff <- paste0(fun_str, bd)
+    fun <- eval(parse(text=ff))
+    return(fun)
+}
+
+expr_to_string <- function(e){
+    e_str <- lapply(e, deparse)
+
+    e_str <-
+        lapply(e_str,
+           function(e){
+               paste0(e, collapse=" ")               
+           })
+    e_str
+}
+
+expr_to_multi_param_fun <- function(e){
+    nms <- all.vars(e)
+
+    e_str <- expr_to_string()
+    
+    fun_str <- paste0("function(", paste0(nms, collapse=", "), ")")
+    
+    
+    bd <- paste0("\n{ \n", paste0(e_str, collapse=";\n "), "\n}")
+    ff <- paste0(fun_str, bd)
+    fun <- eval(parse(text=ff))
+    return(fun)
+}
+
+## expr_to_fun <- function(e){
+    ## vn <- all.vars(e)
+    ## fmls <- vector("list", length(vn))
+    ## names(fmls) <- vn
+    
+    ## out <- function(){}
+
+    ## formals(out) <- fmls
+    ## body(out) <- e
+    ## return(out)
+## }
