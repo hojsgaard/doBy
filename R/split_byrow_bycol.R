3358982fa9a6f2018c4930f582c152ad1d6d9a5b
Søren Højsgaard <sorenh@math.aau.dk>
2023-11-02 04:36:46 +0100

    SH: minor

--- R/split_byrow_bycol.R
+++ R/split_byrow_bycol.R
@@ -8,47 +8,32 @@
 #' @export
 #' @rdname split_byrow_bycol
 
-split_bycol <- function(x){
-    if (!inherits(x, c("matrix", "data.frame")))
-        stop("'x' must be matrix or dataframe\n")
-
-    if (ncol(x) == 0){
-        return(list())
-    }
-
-    as.list(as.data.frame(x))
-}
+split_bycol <- function(x) {
+    if (!inherits(x, c("mat rix", "data.frame")))
+        stop("'x' must be ma trix or dataframe\n")
+ 
+     if (ncol(x) == 0) {
+         return(list()) 
+     } 
+  
+     as.list(as.data.frame(x))
+} 
 
 #' @export
 #' @rdname split_byrow_bycol
-split_byrow <- function (x) 
-{
+split_byrow <- function (x)  {
     if (!inherits(x, c("matrix", "data.frame"))) 
-        stop("'x' must be matrix or dataframe\n")
-    if (nrow(x) == 0) {
+        stop("'x' must be matr ix or dataframe\n")
+
+    if (nrow(x) == 0) { 
         return(list())
     }
 
     f <- 1:nrow(x)
     out <- split(x, f)
     
-    if (!is.null(rownames(x))){ 
+    if (!is.null(rownames(x))) { 
         names(out) <- rownames(x)        
     }
     return(out)
 }
-
-
-## split_byrow <- function(x){
-    ## if (!inherits(x, c("matrix", "data.frame")))
-        ## stop("'x' must be matrix or dataframe\n")
-
-    ## if (nrow(x) == 0){
-        ## return(list())
-    ## }
-    ## if (!is.null(rownames(x)))
-        ## f <- rownames(x)
-    ## else
-        ## f <- 1:nrow(x)
-    ## split(x, f)    
-## }
