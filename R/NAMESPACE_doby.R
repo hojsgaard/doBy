671beccd3f200e8d4551356dbf74abc83e004b18
Søren Højsgaard <sorenh@math.aau.dk>
2024-03-16 07:56:54 +0100

    SH: plot_lm

--- R/NAMESPACE_doby.R
+++ R/NAMESPACE_doby.R
@@ -8,13 +8,14 @@
 #' @importFrom stats as.formula coef delete.response family fitted
 #'     formula ftable lm logLik median model.frame model.matrix pchisq pnorm
 #'     pt qnorm qt residuals sd summary.lm terms vcov aggregate
-#'     printCoefmat confint
+#'     printCoefmat confint predict resid rstandard
 
 #' @importFrom microbenchmark microbenchmark
 #' @importFrom Deriv Deriv
 #' @importFrom magrittr "%>%"
 #' @export  "%>%"
-#' @importFrom utils str
+#' @importFrom utils str help
 #' @importFrom methods setOldClass
+#' @importFrom cowplot plot_grid
 NULL
 
