5264fdf65652e810ef7d684da26cc879402b9efb
Søren Højsgaard <sorenh@math.aau.dk>
2026-07-01 08:55:42 +0200

    SH: doBy 4.7.2 submitted again

--- R/DATA_doby.R
+++ R/DATA_doby.R
@@ -271,7 +271,9 @@ NULL
 #' @references Ulrich Halekoh, Søren Højsgaard (2014)., A Kenward-Roger
 #'     Approximation and Parametric Bootstrap Methods for Tests in Linear Mixed
 #'     Models - The R Package pbkrtest., Journal of Statistical Software,
-#'     58(10), 1-30., \url{https://www.jstatsoft.org/v59/i09/}
+#'     58(10), 1-30.
+#'
+# ## , \url{https://www.jstatsoft.org/v59/i09/}
 #'
 #' @keywords datasets
 #'
